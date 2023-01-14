7550c0fb030cbb11b653db0942dce03498be903c
Skyey <lyfylp@126.com>
2023-01-14 20:28:28 +0800

    Restart

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"github.com/RaymondCode/simple-demo/service"
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func main() {
-	go service.RunMessageServer()
-
-	r := gin.Default()
-
-	initRouter(r)
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	fmt.Println("Hello World!")
 }
